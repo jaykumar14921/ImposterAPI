52a03410d8f281ef8ddebca606e02a11eb2a9c14
Your Namejaykumar14921@gmail.comjay <you@example.com>
2025-04-21 00:17:16 +0530

    fix

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ async function main() {
 
   const client = new MongoClient(uri);
 
+  app.use(express.static(path.join(__dirname, 'public')));
+
   app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   })
